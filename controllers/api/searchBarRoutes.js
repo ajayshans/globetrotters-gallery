5fb9a7da0b7a2efc7f6807eeb3ac31453634f232
mitchmcnaughton <141999182+mitchmcnaughton@users.noreply.github.com>
2023-11-28 20:45:12 +1100

    made search route actually work, fixed errors that would show in console, formatted reponse from search

--- controllers/api/searchBarRoutes.js
+++ controllers/api/searchBarRoutes.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
 const { Landmark } = require('../../models');
-const searchQuery = require('../../public/js/script')
+//const searchQuery = require('../../public/js/script')
 
 
 // Handle GET requests to the /search route
-router.get('/search', async (req, res) => {
-    
+router.get('/', async (req, res) => {
+    const searchQuery = req.query.query
 
     try {
-        const landmarkData = await Landmark.findAll({ where: { location: searchQuery}});  
-
-    res.render('searchbar', { query, landmarkData });
+        const landmarkData = await Landmark.findAll({ where: { location: searchQuery}});
+        const landmarkData2 = landmarkData.map((landmark) => landmark.get({ plain: true}));
+    
+    //res.json(landmarkData2)
+    res.render('searchbar', { searchQuery, landmarkData2 });
     } catch (err) {
         res.status(400).json(err);
     }
