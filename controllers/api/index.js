e06af47281b8581285c86667e14f65a85871a9aa
mitchmcnaughton <141999182+mitchmcnaughton@users.noreply.github.com>
2023-11-30 18:42:18 +1100

    changed view button comments to redirect to comment get route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const searchBarRoutes = require('./searchBarRoutes');
+//const commentRoutes = require('./commentRoutes')
 
 
 router.use('/search', searchBarRoutes);
+//router.use('/comment', commentRoutes)
 
 
 module.exports = router;
