16d05a66e9c15fc347692305e332db93ec3d153c
Ajay S <ajaycshan@gmail.com>
2023-12-04 00:32:40 +1100

    login changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const { Landmark, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', async (req, res) => {
+router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     // Note: findbyPk(1, needs to be changed to findbyPk(req.session.user_id, once login logic is ready
-    const userData = await User.findByPk(1, {
+    const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: Landmark }],
     });
@@ -25,14 +25,14 @@ router.get('/profile', async (req, res) => {
   
 });
 
-// router.get('/login', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect('/profile');
-//     return;
-//   }
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
 
-//   res.render('login');
-// });
+  res.render('login');
+});
 
 module.exports = router;
